c437ad92f39ee956037f476bac18625fb03641aa
Mohamed <mohamed.wally@web.de>
2020-11-03 14:17:53 +0100

    first change

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace Try
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+
         }
     }
 }
